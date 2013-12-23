58f7754c00911dcc9a10cb9a2d3ae37ea59d95b7
Eduardo Jesus <eduardo.fernando.jesus@gmail.com>
2013-12-23 16:32:40 +0000

    Fixed properties union domains in the ontology. Added an experimental query.

--- ontology/src/MusicRDF.java
+++ ontology/src/MusicRDF.java
@@ -1,3 +1,9 @@
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
 import com.hp.hpl.jena.rdf.model.InfModel;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
@@ -30,7 +36,35 @@ public class MusicRDF {
 		
 		InfModel inf_model = ModelFactory.createInfModel(reasoner, data);
 
-		Resource r = inf_model.getResource("http://www.semanticweb.org/dapi/ontologies/music#songf874c56c-fd7f-4098-b7e7-4ed0b395c99b");
-		printStatements(inf_model, r, null, null);
+		//Resource r = inf_model.getResource("http://www.semanticweb.org/dapi/ontologies/music#songf874c56c-fd7f-4098-b7e7-4ed0b395c99b");
+		//printStatements(inf_model, r, null, null);
+		
+		 
+	    /*ValidityReport validity = infmodel.validate();
+	    if (validity.isValid()) {
+	        System.out.println("OK");
+	    } else {
+	        System.out.println("Conflicts");
+	        for (Iterator i = validity.getReports(); i.hasNext(); ) {
+	            System.out.println(" - " + i.next());
+	        }
+	    }*/
+	    
+	    String query_string = 
+	    		"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+	    		"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+	    		"PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+	    		"PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
+	    		"PREFIX dmusic: <http://www.semanticweb.org/dapi/ontologies/music#>\n" +
+	    		"select distinct ?s ?name where {" + //
+	    		"?s dmusic:hasName ?name.\n" +
+	    		"?s rdf:type dmusic:Artist.}";
+	    
+	    Query query = QueryFactory.create(query_string) ;
+	    QueryExecution qexec = QueryExecutionFactory.create(query, inf_model) ;
+	    try {
+	      ResultSet results = qexec.execSelect() ;
+	      ResultSetFormatter.out(System.out, results, query);
+	    } finally { qexec.close() ; }
 	}
 }
