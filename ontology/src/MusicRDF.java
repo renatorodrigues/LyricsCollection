516ff25446dba67ab8180e952f02d99e6f1feb42
Eduardo Jesus <eduardo.fernando.jesus@gmail.com>
2014-01-06 10:45:54 +0000

    Added some individuals to the ontology.

--- ontology/src/MusicRDF.java
+++ ontology/src/MusicRDF.java
@@ -17,7 +17,6 @@ import com.hp.hpl.jena.util.FileManager;
 import com.hp.hpl.jena.util.PrintUtil;
 
 
-
 public class MusicRDF {
 	public static void printStatements(Model m, Resource s, Property p, Resource o) {
 		StmtIterator i = m.listStatements(s,  p, o);
@@ -27,44 +26,51 @@ public class MusicRDF {
 		}
 	}
 	
+	public static void executeQuery(String query_string, InfModel inf_model) {
+		Query query = QueryFactory.create(query_string) ;
+		QueryExecution qexec = QueryExecutionFactory.create(query, inf_model) ;
+		try {
+			ResultSet results = qexec.execSelect() ;
+			ResultSetFormatter.out(System.out, results, query);
+		} finally {
+			qexec.close() ; 
+		}
+	}
+
 	public static void main(String[] args) {	
 		Model schema = FileManager.get().loadModel("music.owl");
 		Model data = FileManager.get().loadModel("music.rdf");
-		
+
 		Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
 		reasoner.bindSchema(schema);
-		
+
 		InfModel inf_model = ModelFactory.createInfModel(reasoner, data);
 
-		//Resource r = inf_model.getResource("http://www.semanticweb.org/dapi/ontologies/music#songf874c56c-fd7f-4098-b7e7-4ed0b395c99b");
-		//printStatements(inf_model, r, null, null);
+		String prefixes ="PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+				"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+				"PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+				"PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
+				"PREFIX dmusic: <http://www.semanticweb.org/dapi/ontologies/music#>\n"; 
 		
-		 
-	    /*ValidityReport validity = infmodel.validate();
-	    if (validity.isValid()) {
-	        System.out.println("OK");
-	    } else {
-	        System.out.println("Conflicts");
-	        for (Iterator i = validity.getReports(); i.hasNext(); ) {
-	            System.out.println(" - " + i.next());
-	        }
-	    }*/
-	    
-	    String query_string = 
-	    		"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-	    		"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
-	    		"PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
-	    		"PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
-	    		"PREFIX dmusic: <http://www.semanticweb.org/dapi/ontologies/music#>\n" +
-	    		"select distinct ?s ?name where {" + //
-	    		"?s dmusic:hasName ?name.\n" +
-	    		"?s rdf:type dmusic:Artist.}";
-	    
-	    Query query = QueryFactory.create(query_string) ;
-	    QueryExecution qexec = QueryExecutionFactory.create(query, inf_model) ;
-	    try {
-	      ResultSet results = qexec.execSelect() ;
-	      ResultSetFormatter.out(System.out, results, query);
-	    } finally { qexec.close() ; }
+		
+		String query_string = prefixes +
+				"select ?artist where { " +
+				"?artist dmusic:worksWith dmusic:EMI ." +
+				"{" +
+				"?artist dmusic:hasGenre dmusic:Rock" +
+				"} UNION " +
+				"{" +
+				"?artist dmusic:hasGenre ?genre ." +
+				"?genre dmusic:subGenreOf dmusic:Rock}}";
+		executeQuery(query_string, inf_model);
+		
+		query_string = prefixes +
+				"select ?s  where {\n"+
+					"?s rdf:type dmusic:Song.\n"+
+					"?s dmusic:hasGenre ?g.\n"+
+					"?g dmusic:subGenreOf dmusic:Rock.}";	
+		executeQuery(query_string, inf_model);
+		
 	}
+
 }
